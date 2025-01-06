ca8909fc5c98b104dc84fcfd62f2fbedf12b0b0e
CodingBeagle <kasper.rie@gmail.com>
2025-01-06 22:28:45 +0100

    initial commit!

---
+++ sources/roguelike-x.cpp
@@ -0,0 +1,19 @@
+﻿// SDL_main should only be included from a single file
+#include <SDL3/SDL_main.h>
+
+#include "roguelike-x.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Hello SDL!" << endl;
+
+	if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != true)
+	{
+		cout << "Failed to initialize SDL!" << endl;
+		SDL_Quit();
+	}
+
+	return 0;
+}
